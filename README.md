e759ca4b46ddd17ea53162104bf87124039bdb93
Devarshi Balu <studious.devarshibalu@gmail.com>
2024-09-02 18:19:43 +0530

    added new files

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 This is demo for Git &amp; Github class.
 
 This is devarshi balu
+
+# teacher 
+Shradha Khapra 
+
+#student 
+Devarshi Balu
---
+++ index.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    console.log('num 1 : ' + a);
+    console.log('num 2 : '+ b );
+    console.log('sum : ' + (a+b)) ;
+}
